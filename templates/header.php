d250e84208546ae9de240976d6710ced62546ffd
Bruno Santos <bruno.santos1996@gmail.com>
2017-02-23 20:43:09 +0000

    added logo

--- templates/header.php
+++ templates/header.php
@@ -6,74 +6,73 @@
     <link rel="stylesheet" href="../style/footer.css">
     <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="../lib/font-awesome/css/font-awesome.css">
+	<script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+	<script src="../lib/jquery/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
 </head>
-
 <body>
-<header>
-    <!--Main nav-->
-    <nav class="navbar navbar-default" id="main-nav">
-        <div class="container-fluid">
-            <div class="row">
-                <!--Logo-->
-                <div class="nav-content col-xs-3 col-sm-3 col-md-3 col-lg-3 navbar-header" id="logo">
-                    <a class="navbar-brand" href="#">.bat</a>
-                </div>
+	<header>
+		<!--Main nav-->
+		<nav class="navbar navbar-default" id="main-nav">
+			<div class="container-fluid">
+				<div class="row">
+					<!--Logo-->
+					<div class="nav-content col-xs-3 col-sm-3 col-md-3 col-lg-3 navbar-header" id="logo">
+						<a class="navbar-brand" href="#">
+							<img src="../resources/logo/logo.png" height="42px" class="hidden-xs hidden-sm">
+							<img src="../resources/logo/logo-mobile.png" height="42px" class="visible-xs-inline-block visible-sm-inline-block">
+						</a>
+					</div>
+					
+					<!--Search bar-->
+					<div class="nav-content col-xs-6 col-sm-6 col-md-6 col-lg-6" id="search-div">
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Search">
+							<div class="input-group-btn">
+								<button class="btn btn-default" id="search-bttn" type="submit">
+									<span class="glyphicon glyphicon-search"></span>
+								</button>
+							</div>
+						</div>
+					</div>
+					
+					<!--Menu-->
+					<div class="nav-content col-xs-3 col-sm-3 col-md-3 col-lg-3" id="menu" align="right">
+						<div class="dropdown visible-xs-inline-block visible-sm-inline-block">
+							<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><span class="glyphicon glyphicon-menu-hamburger"></button>
+							<!-- MAKE MODAL INSTEAD OF DROP DOWN
+							
+							<ul class="dropdown-menu dropdown-menu-right">
+								<li><a href="#"><span class="glyphicon glyphicon-user"></a></li>
+								<li><a href="#"><span class="glyphicon glyphicon-heart"></a></li>
+								<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></a></li>
+							</ul>
+							-->
+						</div>
 
-                <!--Search bar-->
-                <div class="nav-content col-xs-6 col-sm-6 col-md-6 col-lg-6" id="search-div">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                        <div class="input-group-btn">
-                            <button class="btn btn-default" id="search-bttn" type="submit">
-                                <span class="glyphicon glyphicon-search"></span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <!--Menu-->
-                <div class="nav-content col-xs-3 col-sm-3 col-md-3 col-lg-3" id="menu" align="right">
-                    <div class="dropdown visible-xs-inline-block visible-sm-inline-block">
-                        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><span class="glyphicon glyphicon-menu-hamburger"></button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#"><span class="glyphicon glyphicon-user"></a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-heart"></a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></a></li>
-                        </ul>
-                    </div>
-
-                    <ul class="nav navbar-nav navbar-right hidden-xs hidden-sm">
-                        <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
-                        <li><a href="#"><span class="glyphicon glyphicon-heart"></span></a></li>
-                        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
-                        <!--
-                        <li>
-                            <div class="form-group">
-                                <select class="form-control">
-                                    <option><img src="portugal.png" class="img-rounded" alt="portuguese flag"></option>
-                                    <option><img src="united-kingdom.png" class="img-rounded" alt="british flag"></option>
-                                </select>
-                            </div>
-                        </li>
-                        -->
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
-
-    <!--Categories-->
-    <nav class="navbar navbar-default hidden-xs" id="secondary-nav">
-        <ul class="nav navbar-nav">
-            <li><a href="#">Smartphones</a></li>
-            <li><a href="#">Tablets</a></li>
-            <li><a href="#">Peripherals</a></li>
-            <li><a href="#">Computers</a></li>
-            <li><a href="#">Gaming</a></li>
-            <li><a href="#">Cameras</a></li>
-            <li><a href="#">Accessories</a></li>
-        </ul>
-    </nav>
-<header>
+						<ul class="nav navbar-nav navbar-right hidden-xs hidden-sm">
+							<li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+							<li><a href="#"><span class="glyphicon glyphicon-heart"></span></a></li>
+							<li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</nav>
+		
+		<!--Categories-->
+		<nav class="navbar navbar-default hidden-xs" id="secondary-nav">
+			<ul class="nav navbar-nav">
+				<li><a href="#">Smartphones</a></li>
+				<li><a href="#">Tablets</a></li>
+				<li><a href="#">Peripherals</a></li>
+				<li><a href="#">Computers</a></li>
+				<li><a href="#">Gaming</a></li>
+				<li><a href="#">Cameras</a></li>
+				<li><a href="#">Accessories</a></li>
+			</ul>
+		</nav>
+	<header>
+</body>
+</html>
