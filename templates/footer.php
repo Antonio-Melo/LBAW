4e3048b2526af6b6bc2804e06831589d57b1adf8
Antonio-Melo <happyd3nt@gmail.com>
2017-02-24 13:23:19 +0000

    fixing footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,62 +1,62 @@
         <footer>
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-3">
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
                         <h4>CONNECT</h4>
                         <button type="button" class="btn btn-primary"> <span class="glyphicon glyphicon-envelope"></span> Sign up for newsletter</button>
                     </div>
-                    <div class="col-sm-3">
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
                         <h4>Follow us</h4>
                         <div class="row">
-                            <div class="col-sm-2">
+                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                 <a class="btn btn-social-icon btn-instagram">
                                     <span class="fa fa-instagram"></span>
                                 </a>
                             </div>
-                            <div class="col-sm-2">
+                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                 <a class="btn btn-social-icon btn-facebook">
                                     <span class="fa fa-facebook"></span>
                                 </a>
                             </div>
-                            <div class="col-sm-2">
+                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
                                 <a class="btn btn-social-icon btn-twitter">
                                     <span class="fa fa-twitter"></span>
                                 </a>
                             </div>
-                            <div class="col-sm-2">
-                                <a class="btn btn-social-icon btn-youtube">
-                                    <span class="fa fa-youtube"></span>
+                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                                <a class="btn btn-social-icon btn-youtube-play">
+                                    <span class="fa fa-youtube-play"></span>
                                 </a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-3">
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
                         <h4>Costumer Service</h4>
                         <div class="row">
-                            <div class="col-sm-1"></div>
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
                             <a href="#">Terms & Conditions</a>
                         </div>
                         <div class="row">
-                            <div class="col-sm-1"></div>
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
                             <a href="#">Contact Us</a>
                         </div>
                         <div class="row">
-                            <div class="col-sm-1"></div>
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
                             <a href="#">FAQ's</a>
                         </div>
                     </div>
-                    <div class="col-sm-3">
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
                         <h4>About .bat</h4>
                         <div class="row">
-                            <div class="col-sm-1"></div>
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
                             <a href="#">About us</a>
                         </div>
                         <div class="row">
-                            <div class="col-sm-1"></div>
+                            <div class="col-lg-1 col-md-1 sol-sm-1 col-xs-1"></div>
                             <a href="#">Careers</a>
                         </div>
                         <div class="row">
-                            <div class="col-sm-1"></div>
+                            <div class="col-lg-1 col-md-1 sol-sm-1 col-xs-1"></div>
                             <a href="#">Sitemap</a>
                         </div>
                     </div>
