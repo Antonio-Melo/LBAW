5c01e24957db350bca5a425e7373de70ace428fd
Antonio-Melo <happyd3nt@gmail.com>
2017-03-02 19:11:14 +0000

    Doing product

--- pages/search.php
+++ pages/search.php
@@ -1,5 +1,6 @@
 <?php
   $cssPath = '../style/search.css';
+  $jqueryPath = '../scripts/jquery.js';
   include('../templates/header.php');
   include('../templates/search.php');
   include('../templates/footer.php');
