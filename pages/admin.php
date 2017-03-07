fefcb297c144d7c92a1b7f092398496ea0973dae
Antonio-Melo <happyd3nt@gmail.com>
2017-03-07 21:19:41 +0000

    Starting admin page

---
+++ pages/admin.php
@@ -0,0 +1,7 @@
+<?php
+    $cssPath = '../style/admin.css';
+    //$jsPath = '../scripts/admin.js';
+    include('../templates/header.php');
+    include('../templates/admin.php');
+    include('../templates/footer.php');
+?>
---
+++ templates/admin.php
@@ -0,0 +1,8 @@
+<div class="admin-content">
+    <div class="admin-profile">
+
+    </div>
+    <div class="admin-actions">
+        <button type="button" class="btn btn-success btn-xlarge" id="add-product">Success</button>
+    </div>
+</div>
