19dc2f2c8a4a18a453677431f42d85b5c121da8b
Unknown <ruimiguelalmeidaoliveira@gmail.com>
2017-03-07 23:34:52 +0000

    Profile sketch

---
+++ templates/myAddresses.php
@@ -0,0 +1,39 @@
+<myAddresses>
+    <ul class="row list-unstyled">
+        <li class="col-sm-6 col-md-6">
+            <div class="adressCard">
+                <p>Augustus Caesar</p>
+                <p>Street 1, Door 1</p>
+                <p>Roma 1000</p>
+                <p>Roma</p>
+                <p>Roman Empire</p>
+                <p>Tel:+39 06 6485 0987</p>
+                <ul class="row list-unstyled left editable">
+                    <li class="list-group-item"><a href="#"><i class="fa fa-trash"></i></a></li>
+                    <li class="list-group-item"><a href="#"><i class="fa fa-pencil"></i></a></li>
+                    <li class="list-group-item"><a href="#"><i>(set as default)</i></a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="col-sm-6 col-md-6">
+            <div class="adressCard">
+                <p>Cleopatra</p>
+                <p>Street 1, Door 1</p>
+                <p>Cairo 1000</p>
+                <p>Cairo</p>
+                <p>Egyptian Empire</p>
+                <p>Tel:+20 101 701 7003</p>
+                <ul class="row list-unstyled left editable">
+                    <li class="list-group-item"><a href="#"><i class="fa fa-trash"></i></a></li>
+                    <li class="list-group-item"><a href="#"><i class="fa fa-pencil"></i></a></li>
+                    <li class="list-group-item"><a href="#"><i>(set as default)</i></a></li>
+                </ul>
+            </div>
+        </li>
+
+
+        <li class="col-sm-6 col-md-6">
+            <button type="button" class="btn btn-primary btn-block profileButton"> <i class="fa fa-plus"></i> Add Address</button>
+        </li>
+    </ul>
+</myAddresses>
