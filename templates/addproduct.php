e05c662459e8a775058a34c5a4d794cf1e7b1a63
Antonio-Melo <happyd3nt@gmail.com>
2017-03-08 14:13:37 +0000

    add product done(missing css to center)

--- templates/addproduct.php
+++ templates/addproduct.php
@@ -3,15 +3,15 @@
         <h4>New Product</h4>
     </div>
 
-    <div class="input-group col-lg-12 col-md-12">
+    <div class="input-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="row">
-            <div class="name col-lg-6 col-md-6">
+            <div class="name col-lg-6 col-md-6 col-sm-12 col-xs-12">
                 <span class="input-group-addon"><i class="glyphicon glyphicon-tag"> Name</i></span>
-                <input type="text" class="form-control" id="usr">
+                <input type="text" class="form-control" id="name">
             </div>
         </div>
         <div class="row">
-            <div class="small-description col-lg-6 col-md-6">
+            <div class="small-description col-lg-6 col-md-6 col-sm-12 col-xs-12">
                 <div class="form-group">
                     <span class="input-group-addon"><i class="glyphicon glyphicon-tags"> Small description</i></span>
                     <textarea class="form-control" rows="5" id="comment"></textarea>
@@ -19,7 +19,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="form-group col-lg-6 col-md-6">
+            <div class="form-group col-lg-6 col-md-6 col-sm-12 col-xs-12">
                 <span class="input-group-addon"><i class="glyphicon glyphicon-list"> Type</i></span>
                 <select class="form-control" id="types">
                     <option>Smartphone</option>
@@ -30,6 +30,53 @@
                     <option>Camera</option>
                     <option>Accessories</option>
                 </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="name col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <span class="input-group-addon"><i class="fa fa-money"> Price</i></span>
+                <input type="number" class="form-control" id="price">
+            </div>
+        </div>
+        <div class="row">
+            <div class="colors col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <form>
+                    <label class="checkbox-inline">
+                        <input type="checkbox" value="">Black
+                    </label>
+                    <label class="checkbox-inline">
+                        <input type="checkbox" value="">White
+                    </label>
+                    <label class="checkbox-inline">
+                        <input type="checkbox" value="">Golden
+                    </label>
+                    <label class="checkbox-inline">
+                        <input type="checkbox" value="">Silver
+                    </label>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="name col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <span class="input-group-addon"><i class="fa fa-coins" >Quantity</i></span>
+                <input type="number" class="form-control" id="qty">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-sm-6 col-12">
+                <span class="input-group-addon"><i class="glyphicon glyphicon-cargo"> Images</i></span>
+                <label class="btn btn-primary">
+                    Browse&hellip; <input type="file" style="display: none;">
+                </label>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="small-description col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="form-group">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-tags"> Full description</i></span>
+                    <textarea class="form-control" rows="10" id="comment"></textarea>
+                </div>
             </div>
         </div>
     </div>
