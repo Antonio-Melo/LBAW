09c129168b9187e2a53f219160a93954ed60a980
Bruno Santos <bruno.santos1996@gmail.com>
2017-03-08 19:17:01 +0000

    profile orders done

--- templates/profile.php
+++ templates/profile.php
@@ -24,16 +24,6 @@
 					<h2> <i class="fa fa-list-alt" ></i>&nbsp; My Orders</h2>
 				</a>
 			</li>
-			<li>
-				<a data-toggle="pill" href="#supportTicket">
-					<h2> <i class="fa fa-sticky-note-o"></i>&nbsp; Support Ticket</h2>
-				</a>
-			</li>
-			<li>
-				<a data-toggle="pill" href="#myOrders">
-					<h2> <i class="fa fa-refresh" ></i>&nbsp; Service Requests</h2>
-				</a>
-			</li>
 		</ul>
 	
 		<div class="tab-content">
@@ -154,19 +144,268 @@
 
 			<!-- My Orders -->
 			<div id="myOrders" class="tab-pane">
-				444444444444
-			</div>
+				<div class="list-group panel" id="orders">
+					<!--ORDER-->
+					<div class="container-fluid">
+						<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+							<a href="#order1" class="list-group-item" data-toggle="collapse" id="orderNumber">
+								Order Number: BAT00710235
+								<i class="fa fa-caret-down"></i>
+							</a>
+						</div>
+						<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" align="right">
+							<h5 class="orderDate">Sep 12, 2016 09:10:22 AM</h5>
+						</div>
+					</div>
+					<div class="collapse order" id="order1">
+						<!--TRACKING-->
+						<div class="tracking">
+							<div class="row">
+								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" align="center" id="filler">
+									<i class="fa fa-check-square-o" style="font-size:25px"></i>
+									<h5>Order Placed</h5>
+								</div>
+								<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" align="center">
+									<i class="fa fa-money" style="font-size:25px"></i>
+								</div>
+								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" align="center">
+									<i class="fa fa fa-check-square-o" style="font-size:25px"></i>
+									<h5>Payment Received</h5>
+								</div>
+								<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" align="center">
+									<i class="fa fa-gift" style="font-size:25px"></i>
+								</div>
+								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" align="center">
+									<i class="fa fa fa-check-square-o" style="font-size:25px"></i>
+									<h5>Dispatched</h5>
+								</div>
+								<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" align="center">
+									<i class="fa fa-plane" style="font-size:25px"></i>
+								</div>
+								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" align="center">
+									<i class="fa fa fa-check-square-o" style="font-size:25px"></i>
+									<h5>Delivered</h5>
+								</div>
+							</div>
+						</div>
+						<!--SHIPPING INFO-->
+						<div class="shippingInfo">
+							<div class="row">
+								<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+									<p>Shipping Method</p>
+									<p>Standard</p>
+									<p>Payment Method</p>
+									<p>Paypal</p>
+								</div>
+								<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+									<div>
+										<p>Augustus Caesar</p>
+										<p>Street 1, Door 1</p>
+										<p>Roma 1000</p>
+										<p>Roma</p>
+										<p>Roman Empire</p>
+										<p>Tel:+39 06 6485 0987</p>
+									</div>
+								</div>
+								<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+									<div>
+										<p>Augustus Caesar</p>
+										<p>Street 1, Door 1</p>
+										<p>Roma 1000</p>
+										<p>Roma</p>
+										<p>Roman Empire</p>
+										<p>Tel:+39 06 6485 0987</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!--SHIPPING CONTENTS-->
+						<div class="shippingContents">
+							<?php
+							$n_products = 5;
 
-			<!-- Support Ticket -->
-			<div id="supportTicket" class="tab-pane">
-				555555555555
-			</div>
+							for ($i = 0; $i < $n_products; $i++) {
+								?>
+								<div class="product-list">
+									<div class="product-image-container">
+										<img src="../resources/products/xiaomi_mi5.png" alt="xiaomi_mi5">
+									</div>
+									<div class="product-info-container">
+										<div class="row">
+											<div class="list-left-container col-lg-6 col-md-6 col-sm-6 col-xs-12">
+												<div class="name"><a>Xiaomi MI5</a></div>
+												<div class="type-brand"><a>Smartphone</a> - <a>Xiaomi</a></div>
+											</div>
+											<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+												<div class="price"><span class="price-value">300,00</span>€</div>
+											</div>
+											<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+												<div class="quantity"><span class="price-value">x 1</span></div>
+											</div>
+											<div class="list-right-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+												<div class="price"><span class="price-value">300,00</span>€</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<?php
+							}
+							?>
+						</div>
+						<!--SHIPPING PRICE-->
+						<div class="shippingPrice">
+							<div class="row">
+								<div class="list-left-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+									<div class="price">Subtotal: <span class="price-value">1500,00</span>€</div>
+								</div>
+								<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+									<div class="price">Discount: <span class="price-value">0,00</span>€</div>
+								</div>
+								<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+									<div class="price">Tax: <span class="price-value">0,00</span>€</div>
+								</div>
+								<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+									<div class="price">Shipping: <span class="price-value">0,00</span>€</div>
+								</div>
+								<div class="list-middle-container col-lg-4 col-md-4 col-sm-4 col-xs-12" align="right">
+									<div class="price">Grand Total: <span class="price-value">1500,00</span>€</div>
+								</div>
+							</div>
+						</div>
+					</div>
 
-			<!-- Service Requests -->
-			<div id="serviceRequests" class="tab-pane">
-				666666666666
-			</div>
 
+
+					<!--ORDER-->
+					<div class="container-fluid">
+						<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9">
+							<a href="#order2" class="list-group-item" data-toggle="collapse" id="orderNumber">
+								Order Number: BAT00905742
+								<i class="fa fa-caret-down"></i>
+							</a>
+						</div>
+						<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" align="right">
+							<h5 class="orderDate">Nov 28, 2016 20:23:41 AM</h5>
+						</div>
+					</div>
+					<div class="collapse order" id="order2">
+						<!--TRACKING-->
+						<div class="tracking">
+							<div class="row">
+								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" align="center" id="filler">
+									<i class="fa fa-check-square-o" style="font-size:25px"></i>
+									<h5>Order Placed</h5>
+								</div>
+								<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" align="center">
+									<i class="fa fa-money" style="font-size:25px"></i>
+								</div>
+								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" align="center">
+									<i class="fa fa fa-check-square-o" style="font-size:25px"></i>
+									<h5>Payment Received</h5>
+								</div>
+								<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" align="center">
+									<i class="fa fa-gift" style="font-size:25px"></i>
+								</div>
+								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" align="center">
+									<i class="fa fa fa-check-square-o" style="font-size:25px"></i>
+									<h5>Dispatched</h5>
+								</div>
+								<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" align="center">
+								</div>
+								<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" align="center">
+									<i class="fa fa fa-square-o" style="font-size:25px"></i>
+									<h5>Delivered</h5>
+								</div>
+							</div>
+						</div>
+						<!--SHIPPING INFO-->
+						<div class="shippingInfo">
+							<div class="row">
+								<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+									<p>Shipping Method</p>
+									<p>Standard</p>
+									<p>Payment Method</p>
+									<p>Paypal</p>
+								</div>
+								<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+									<div>
+										<p>Augustus Caesar</p>
+										<p>Street 1, Door 1</p>
+										<p>Roma 1000</p>
+										<p>Roma</p>
+										<p>Roman Empire</p>
+										<p>Tel:+39 06 6485 0987</p>
+									</div>
+								</div>
+								<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+									<div>
+										<p>Augustus Caesar</p>
+										<p>Street 1, Door 1</p>
+										<p>Roma 1000</p>
+										<p>Roma</p>
+										<p>Roman Empire</p>
+										<p>Tel:+39 06 6485 0987</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!--SHIPPING CONTENTS-->
+						<div class="shippingContents">
+							<?php
+							$n_products = 5;
+
+							for ($i = 0; $i < $n_products; $i++) {
+								?>
+								<div class="product-list">
+									<div class="product-image-container">
+										<img src="../resources/products/xiaomi_mi5.png" alt="xiaomi_mi5">
+									</div>
+									<div class="product-info-container">
+										<div class="row">
+											<div class="list-left-container col-lg-6 col-md-6 col-sm-6 col-xs-12">
+												<div class="name"><a>Xiaomi MI5</a></div>
+												<div class="type-brand"><a>Smartphone</a> - <a>Xiaomi</a></div>
+											</div>
+											<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+												<div class="price"><span class="price-value">300,00</span>€</div>
+											</div>
+											<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+												<div class="quantity"><span class="price-value">x 1</span></div>
+											</div>
+											<div class="list-right-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+												<div class="price"><span class="price-value">300,00</span>€</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<?php
+							}
+							?>
+						</div>
+						<!--SHIPPING PRICE-->
+						<div class="shippingPrice">
+							<div class="row">
+								<div class="list-left-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+									<div class="price">Subtotal: <span class="price-value">1500,00</span>€</div>
+								</div>
+								<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+									<div class="price">Discount: <span class="price-value">0,00</span>€</div>
+								</div>
+								<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+									<div class="price">Tax: <span class="price-value">0,00</span>€</div>
+								</div>
+								<div class="list-middle-container col-lg-2 col-md-2 col-sm-2 col-xs-12">
+									<div class="price">Shipping: <span class="price-value">0,00</span>€</div>
+								</div>
+								<div class="list-middle-container col-lg-4 col-md-4 col-sm-4 col-xs-12" align="right">
+									<div class="price">Grand Total: <span class="price-value">1500,00</span>€</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
 		</div>
     </div>
 </div>
