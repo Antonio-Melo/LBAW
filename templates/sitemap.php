be029c7b22ff3ce53e15061fe4fd88d33e90f668
Bruno Santos <bruno.santos1996@gmail.com>
2017-03-08 18:20:15 +0000

    chevrons on sitemap

--- templates/sitemap.php
+++ templates/sitemap.php
@@ -3,24 +3,24 @@
 	<h1>Sitemap</h1>
 	<hr>
 	<div class="sitemap-content">
-		<p><a href="home.php">Home</a></p>
+		<p><a href="home.php"><span class="glyphicon glyphicon-chevron-right"></span> Home</a></p>
 		<h2>Catalog</h2>
-		<p><a href="product.php">Product</a></p>
-		<p><a href="category.php">Category</a></p>
-		<p><a href="search.php">Search</a></p>
+		<p><a href="product.php"><span class="glyphicon glyphicon-chevron-right"></span> Product</a></p>
+		<p><a href="category.php"><span class="glyphicon glyphicon-chevron-right"></span> Category</a></p>
+		<p><a href="search.php"><span class="glyphicon glyphicon-chevron-right"></span> Search</a></p>
 		<h2>User</h2>
-		<p><a href="profile.php">Profile</a></p>
-		<p><a href="favorites.php">Favorites</a></p>
-		<p><a href="cart.php">Cart</a></p>
+		<p><a href="profile.php"><span class="glyphicon glyphicon-chevron-right"></span> Profile</a></p>
+		<p><a href="favorites.php"><span class="glyphicon glyphicon-chevron-right"></span> Favorites</a></p>
+		<p><a href="cart.php"><span class="glyphicon glyphicon-chevron-right"></span> Cart</a></p>
 		<h2>Admin</h2>
-		<p><a href="addproduct.php">Add Product</a></p>
-		<p><a href="admin-stats.php">Admin Stats</a></p>
-		<p><a href="ban-users.php">Ban Users</a></p>
+		<p><a href="addproduct.php"><span class="glyphicon glyphicon-chevron-right"></span> Add Product</a></p>
+		<p><a href="admin-stats.php"><span class="glyphicon glyphicon-chevron-right"></span> Admin Stats</a></p>
+		<p><a href="ban-users.php"><span class="glyphicon glyphicon-chevron-right"></span> Ban Users</a></p>
 		<h2>Customer service</h2>
-		<p><a href="terms-conditions.php">Terms & Conditions</a></p>
-		<p><a href="contact-us.php">Contact Us</a></p>
-		<p><a href="faqs.php">FAQs</a></p>
-		<p><a href="about-us.php">About Us</a></p>
-		<p><a href="sitemap.php">Sitemap</a></p>
+		<p><a href="terms-conditions.php"><span class="glyphicon glyphicon-chevron-right"></span> Terms & Conditions</a></p>
+		<p><a href="contact-us.php"><span class="glyphicon glyphicon-chevron-right"></span> Contact Us</a></p>
+		<p><a href="faqs.php"><span class="glyphicon glyphicon-chevron-right"></span> FAQs</a></p>
+		<p><a href="about-us.php"><span class="glyphicon glyphicon-chevron-right"></span> About Us</a></p>
+		<p><a href="sitemap.php"><span class="glyphicon glyphicon-chevron-right"></span> Sitemap</a></p>
 	</div>
 </div>
